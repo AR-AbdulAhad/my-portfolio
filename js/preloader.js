cc157fef61d9a352fbd3076d129c5050c69e19df
Abdul Ahad <124049277+AR-AbdulAhad@users.noreply.github.com>
2024-04-04 03:04:11 +0500

    update

---
+++ js/preloader.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Hide the preloader when the DOM content is fully loaded
+  var preloader = document.getElementById('preloader');
+  preloader.style.display = 'none';
+});
